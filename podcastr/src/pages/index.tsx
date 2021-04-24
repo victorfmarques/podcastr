e3f710a209f51ef872e7e3bee4e309fd5af6c276
Victor Marques <victorfmarques@outlook.com>
2021-04-24 17:32:12 -0300

    Setting fake API and SSG

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -3,3 +3,15 @@ export default function Home() {
    <p>Index</p>
   );
 }
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60*60*8,
+  }
+}
